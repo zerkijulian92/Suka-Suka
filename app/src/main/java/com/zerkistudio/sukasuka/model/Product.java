2b56a24c385cf8fa746e182115825b8d7442f80e
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-07-31 20:56:27 +0700

    Menambahkan Constructor dan Getter And Setter di Product.java

--- app/src/main/java/com/zerkistudio/sukasuka/model/Product.java
+++ app/src/main/java/com/zerkistudio/sukasuka/model/Product.java
@@ -5,4 +5,45 @@ public class Product {
     private String ProductName;
     private int Quantity;
     private String SKU;
+
+    //Constructor
+    public Product(int productId, String productName, int quantity, String SKU) {
+        ProductId = productId;
+        ProductName = productName;
+        Quantity = quantity;
+        this.SKU = SKU;
+    }
+
+    //Getter and Setter
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(int productId) {
+        ProductId = productId;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public String getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
 }
