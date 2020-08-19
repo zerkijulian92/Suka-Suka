9a42cc57420e51834108ff6aaa9c05c8b37709df
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-08-19 10:56:36 +0700

    Day 4- Antara Android - MySQL DB dan Retrofit (Sesi 3)

--- app/src/main/java/com/zerkistudio/sukasuka/LoginActivity.java
+++ app/src/main/java/com/zerkistudio/sukasuka/LoginActivity.java
@@ -7,6 +7,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.zerkistudio.sukasuka.core.Constant;
+import com.zerkistudio.sukasuka.netservice.IAuthService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +29,17 @@ public class LoginActivity extends AppCompatActivity {
         EditText etPassword = (EditText) findViewById(R.id.et_password);
 
         Toast.makeText(this, "Username: " + etIDPel.getText() + "\n" +"Password: " +etPassword.getText(), Toast.LENGTH_SHORT).show();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constant.JSONURL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        IAuthService authAPIService = retrofit.create(IAuthService.class);
+
+        Map<String, String> mapAuth = new HashMap<String, String>();
+        mapAuth.put("id", etIDPel.getText().toString());
+        mapAuth.put("pass", etPassword.getText().toString());
+
     }
 }
---
+++ app/src/main/java/com/zerkistudio/sukasuka/core/Constant.java
@@ -0,0 +1,5 @@
+package com.zerkistudio.sukasuka.core;
+
+public class Constant {
+    public static String JSONURL = "http://192.168.88.104/sukasuka/api/";
+}
